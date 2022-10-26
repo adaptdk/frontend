75fc17bdbc7793fd1ddb8063556714991ebf09b3
frederiknygaardadapt <frederik.nygaard@adaptagency.com>
2022-10-26 16:17:00 +0200

    get data

--- statamic-nextjs/src/pages/_app.tsx
+++ statamic-nextjs/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@foundation/styles/globals.css";
+import "@foundation/ui/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
