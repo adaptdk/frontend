17ce42382a837fa1a0af3387f991bf7c17ca6e0a
Jakub Wolak <jakub@adaptagency.com>
2022-12-20 10:42:29 +0100

    awesome snow and ugly Christmas reminder

--- u-website/src/App.tsx
+++ u-website/src/App.tsx
@@ -1,16 +1,30 @@
+import { Counter } from "components/Counter";
 import { Footer } from "components/Footer";
 import { Header } from "components/Header";
 import { Hero } from "components/Hero";
 import { NewsBanner } from "components/NewsBanner";
+import Snowfall from "react-snowfall";
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen justify-between">
+      <Snowfall
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+        }}
+        snowflakeCount={750}
+        speed={[7, 10]}
+        wind={[4, 10]}
+        radius={[1, 15]}
+      />
       <div>
         <Header />
         <Hero />
         <NewsBanner />
       </div>
+      <Counter />
       <Footer />
     </div>
   );
