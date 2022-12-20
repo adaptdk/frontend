00cd6477c00b8b9f632a966f2a4550fd88f80594
Agne Krasauskaite <agne@adaptagency.com>
2022-12-20 10:43:49 +0100

    footer

--- u-website/src/components/Footer/Footer.tsx
+++ u-website/src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
+
 export const Footer = () => {
   return (
-    <footer className="bg-blue-900 p-8 grid grid-cols-2 gap-2">
-      <div className="">
+    <footer className="bg-green-900 p-8 grid grid-cols-2 gap-2 bg-footer-pattern bg-center ">
+        <div>
         <div>
           <img src="/logo.svg" alt="Logo" />
         </div>
-        <div className="mt-4 text-white font-semibold flex flex-col space-y-2">
+
+        <div className="mt-4 font-semibold flex flex-col space-y-2 text-yellow-500">
+          <h1 className="text-6xl">From us to you - happy holidays!</h1>
           <a href="/">Kontakt os</a>
           <a href="/">Nyheder</a>
           <a href="/">Privacy policy</a>
@@ -13,9 +16,15 @@ export const Footer = () => {
         </div>
         {/* Maybe some social links here? */}
       </div>
-      <div className="space-x-8">
-        <button className="text-white font-semibold underline">Europa</button>
-        <button className="text-white font-semibold underline">Asien</button>
+      <div className="flex flex-col justify-center items-center">
+        <div className="space-x-8 drop-shadow-2xl">
+        <button className="text-yellow-500 font-semibold underline text-xl">Europa</button>
+        <button className="text-blue-500 font-semibold underline text-5xl">Asien</button>
+        <button className="text-red-500 font-semibold underline text-sm">Africa</button>
+        <button className="text-green-500 font-semibold underline text-2xl">America</button>
+        <button className="text-purple-500 font-semibold underline text-6xl">Australia</button>
+        </div>
+        <div className="mt-8"><img src="/map.jpg" alt="" /></div>
         {/* Maybe a cool map? */}
       </div>
     </footer>
--- u-website/tailwind.config.cjs
+++ u-website/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+            'footer-pattern': "url('/footer.png')",
+          }
+    },
   },
   plugins: [],
 };
