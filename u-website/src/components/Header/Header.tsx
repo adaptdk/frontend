479d76537bdf54dc78cc0419780177a28836b166
meandersen <mathias.andersen@adaptagency.com>
2022-12-20 10:43:56 +0100

    header

--- u-website/src/components/Header/Header.tsx
+++ u-website/src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
   return (
-    <header className="bg-blue-900 p-8 flex justify-between">
+    <header className="bg-[url('/header-bg.jpg')] bg-repeat-x bg-contain p-8 flex justify-between items-center">
       <div>
-        <img src="/logo.svg" alt="Logo" />
+        <img src="/logo.svg" alt="Logo" className="w-auto h-12" />
       </div>
-      <div className="space-x-4 text-white font-semibold">
+      <div className="space-x-4 font-semibold text-red-500">
         <a href="/">Cases</a>
         <a href="/">Om os</a>
         <a href="/">Nyheder</a>
